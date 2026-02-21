8a716cdd32477cb4f84d961a9fc5bae71a5642b4
배민준 <sokk159dps@gmail.com>
2026-02-21 21:01:18 +0900

    [fix] 지 혼자 로딩 돌고 있는거 가림

--- src/components/common/LoadingSpinner.vue
+++ src/components/common/LoadingSpinner.vue
@@ -1,11 +1,19 @@
 <template>
-  <div :class="styles.spinner">
-    <div :class="styles.dot" />
-    <div :class="styles.dot" />
-    <div :class="styles.dot" />
+  <div v-if="isLoading" :class="styles.overlay">
+    <div :class="styles.content">
+      <div :class="styles.spinner" />
+      <p v-if="message" :class="styles.message">{{ message }}</p>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import styles from '@/components/common/LoadingSpinner.css.module.css'
+
+interface Props {
+  isLoading: boolean
+  message?: string
+}
+
+defineProps<Props>()
 </script>
